981e412c9034eff6ec2af57bbf00588627aa4a19
nik-qms <58138097+nik-qms@users.noreply.github.com>
2020-03-23 10:35:56 +0100

    InstallDB-v0
    InstallDB-v0

---
+++ QAMMobile/Models/DTOPUsers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QAMMobile.Models
+{
+    public class DTOPUsers
+    {
+        public long nUserId { get; set; }
+        public string sFirstName { get; set; }
+        public string sLastName { get; set; }
+        public string sAddress1 { get; set; }
+        public string sAddress2 { get; set; }
+        public string sAddress3 { get; set; }
+        public string sPhoneNumber { get; set; }
+        public string sEmailAddress { get; set; }
+        public string sGender { get; set; }
+        public string sTitle { get; set; }
+        public long nCompanyId { get; set; }
+        public long nVerified { get; set; }
+        public string sLicenseNumber { get; set; }
+        public long nLicenseType { get; set; }
+        public string sLicenseIssuedOn { get; set; }
+        public string sLicenseExpiryDate { get; set; }
+        public string sLicenseIssuingAuthority { get; set; }
+        public string sInfractions { get; set; }
+
+    }
+}
